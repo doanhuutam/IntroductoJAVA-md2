60e0f574aad22654b60d273ee0f2545eae86226f
doanhuutam <tam1072002tq@gmail.com>
2021-06-07 15:09:10 +0700

    day1

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello");
+        System.out.println("Hi world!");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+    }
+}
